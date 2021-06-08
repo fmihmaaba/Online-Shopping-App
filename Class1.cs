eab84125e8f5edf81d4e8b06ceca39ca6bc3d1bd
fmihmaaba <82494666+fmihmaaba@users.noreply.github.com>
2021-06-08 20:51:06 +0800

    Add files via upload
    Sa activity po ngayon mam ang ginawa ko po is yung Encapsulation and Abstraction sa class2 ko po.
    
    ang system ko po ay online shopping, sa //previous case2 ko po mam, after po nila mag fill up ng full name, last name, ay ipapakita po ito sa output. pero ngayon po ang ginawa ko para sa Encapsulation ng case 2 ko is gumamit po ako ng get and set properties, at gaya nga ng sabi ko dinagdagan ko po siya ng Address na po. Sa susunod na po yung iba pang i f-fill up ng costumer. Ang ginawa ko po sa Abstraction ay tatanggalin ko na po yung //previous case2 ko po para hindi siya mag mukhang madami pag nilagyan ko na po ng ibat iba pang i f-fill up-an ng mga costumer ko. Thank you po mam, hindi ko pa din po abot yung Inheritance at iniintindi ko pa rin po ito hanggang ngayon.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿{
+    class Program : Capacity
+    {
+        static void Main(string[] args)
+        {
+            List<string> Names = new List<string>();
+            Names.Add("Razer Viper Mini");
+            Names.Add("Logitech G304");
+            Names.Add("ZygenNP - 01");
+            Console.WriteLine("Number of Items in the List is:" + Names.Count);
+            Console.WriteLine("Capacity of the List is:" + Names.Capacity);
+        }
+    }
+}       
---
+++ Class2.cs
@@ -0,0 +1,74 @@
+﻿
+// CURRENT CASE
+
+using System;
+
+using System.Text;
+namespace Customer
+{
+    class User
+    {
+        private string address;
+        private string name;
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User();
+            // set accessor will invoke
+            u.Name = "Franz Maaba";
+            // set accessor will invoke
+            u.Address = "Laguna";
+            // get accessor will invoke
+            Console.WriteLine("Name: " + u.Name);
+            // get accessor will invoke
+            Console.WriteLine("Address: " + u.Address);
+            Console.WriteLine("\nPress Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+// PREVIOUS CASE
+{
+    class Program : Customer
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.FirstName = "Franz";
+            p.LastName = "Maaba";
+            p.FullName();
+            Console.ReadLine();
+
+        }
+        public override void FullName()
+        {
+            Console.WriteLine("Full Name:" + FirstName + " " + LastName);
+        }
+    }
+}
